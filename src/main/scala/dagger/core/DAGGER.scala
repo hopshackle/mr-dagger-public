5aaf88b40e4f13a4387aa92b040c50750307c27a
hopshackle <james@janigo.co.uk>
2015-07-11 15:38:37 +0100

    Put debug messages inside debug

--- src/main/scala/dagger/core/DAGGER.scala
+++ src/main/scala/dagger/core/DAGGER.scala
@@ -73,7 +73,7 @@ class DAGGER[D: ClassTag, A <: TransitionAction[S]: ClassTag, S <: TransitionSta
         // Use policies to fully construct (unroll) instance from start state
         val (predEx, predActions, expertUse) = unroll(d, expert, policy, trans.init(d), trans, features, prob)
         if (options.DEBUG) debug.write("Initial State:" + trans.init(d) + "\n")
-        if (options.DEBUG) debug.write("Actions Taken:\n"); (predActions zip expertUse) foreach (x => debug.write(x._1 + " : " + x._2 + "\n"))
+        if (options.DEBUG) {debug.write("Actions Taken:\n"); (predActions zip expertUse) foreach (x => debug.write(x._1 + " : " + x._2 + "\n"))}
         val loss = lossFactory.newLossFunction
         val totalLoss = predEx match {
           case None => 1.0
